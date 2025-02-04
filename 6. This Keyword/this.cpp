4473998f192e9e963e34c7748a2023f104b98818
Sabrina Mostafa <sabrinamostafa9900@gmail.com>
2025-02-05 02:34:21 +0600

    code of this keyword

---
+++ 6. This Keyword/this.cpp
@@ -0,0 +1,52 @@
+///  ~~~~~Bismillah'hir Rah'maanir Rahim~~~~~  ///
+
+/*  "  Allah(SWT) blesses with knowledge whom he wants. "   */
+/*  " Indeed with Hardship, comes Ease. "    */
+
+
+///         Originator : Sabrina Mostafa
+///         Dept. Of CSE
+///         International Islamic University Chittagong
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std ;
+
+
+
+class Hero {
+public:
+    int health=10 ;  // this == (h1.health)
+
+    Hero(int health) {
+        cout<<"Address of this pointer: "<<this<<endl ;
+        this->health = health ;
+    }
+};
+
+
+int main ()
+{
+    // Current Object = h1
+    Hero h1(30) ;
+
+    cout<<"Address of current Object, h1: "<<&h1<<endl ;
+    cout<<"Value of current Object, h1: "<<h1.health<<endl ;
+
+
+
+
+    return 0 ;
+}
+
+/*
+O/P:
+
+Address of this pointer: 0x61ff0c
+Address of current Object, h1: 0x61ff0c
+
+Value of current Object, h1: 30
+
+*/
